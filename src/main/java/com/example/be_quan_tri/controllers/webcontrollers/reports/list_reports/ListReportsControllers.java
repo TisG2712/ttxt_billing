3ef4108ab6ccecd788a8ddb60d649f021ed21977
Nguyễn Trường Giang <truonggiang27122003@gmail.com>
2025-07-28 14:29:34 +0700

    main

--- src/main/java/com/example/be_quan_tri/controllers/webcontrollers/reports/list_reports/ListReportsControllers.java
+++ src/main/java/com/example/be_quan_tri/controllers/webcontrollers/reports/list_reports/ListReportsControllers.java
@@ -1,6 +1,18 @@
 package com.example.be_quan_tri.controllers.webcontrollers.reports.list_reports;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+@RequestMapping(value = "reports/list_reports/")
 public class ListReportsControllers {
+    @GetMapping("reportSystemTime")
+    public String reportSystemTime(@RequestParam(defaultValue = "" , value = "size") String size,
+                                            @RequestParam(defaultValue = "" , value = "page") String page){
+        System.out.println(size+"/"+page);
+        return "dashboard/reports/list_reports/reportSystemTime";
+    }
 
 }
