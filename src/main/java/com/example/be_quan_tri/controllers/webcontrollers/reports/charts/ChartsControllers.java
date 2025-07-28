6e367606ce5d359c0126a51ac338dc10d6c45551
Nguyễn Trường Giang <truonggiang27122003@gmail.com>
2025-07-28 08:51:17 +0700

    main

--- src/main/java/com/example/be_quan_tri/controllers/webcontrollers/reports/charts/ChartsControllers.java
+++ src/main/java/com/example/be_quan_tri/controllers/webcontrollers/reports/charts/ChartsControllers.java
@@ -14,4 +14,82 @@ public class ChartsControllers {
         System.out.println(size+"/"+page);
         return "dashboard/reports/charts/testEnvServiceStatusChart";
     }
+    @GetMapping("prodEnvServiceStatusChart")
+    public String prodEnvServiceStatusChart(@RequestParam(defaultValue = "" , value = "size") String size,
+                                            @RequestParam(defaultValue = "" , value = "page") String page){
+        System.out.println(size+"/"+page);
+        return "dashboard/reports/charts/prodEnvServiceStatusChart";
+    }
+    @GetMapping("topSharingOrganizationsChart")
+    public String topSharingOrganizationsChart(@RequestParam(defaultValue = "" , value = "size") String size,
+                                            @RequestParam(defaultValue = "" , value = "page") String page){
+        System.out.println(size+"/"+page);
+        return "dashboard/reports/charts/topSharingOrganizationsChart";
+    }
+    @GetMapping("topIntegratedServicesChart")
+    public String topIntegratedServicesChart(@RequestParam(defaultValue = "" , value = "size") String size,
+                                            @RequestParam(defaultValue = "" , value = "page") String page){
+        System.out.println(size+"/"+page);
+        return "dashboard/reports/charts/topIntegratedServicesChart";
+    }
+    @GetMapping("integrationApprovalRateChart")
+    public String integrationApprovalRateChart(@RequestParam(defaultValue = "" , value = "size") String size,
+                                            @RequestParam(defaultValue = "" , value = "page") String page){
+        System.out.println(size+"/"+page);
+        return "dashboard/reports/charts/integrationApprovalRateChart";
+    }
+    @GetMapping("integrationRequestTrendChart")
+    public String integrationRequestTrendChart(@RequestParam(defaultValue = "" , value = "size") String size,
+                                            @RequestParam(defaultValue = "" , value = "page") String page){
+        System.out.println(size+"/"+page);
+        return "dashboard/reports/charts/integrationRequestTrendChart";
+    }
+    @GetMapping("topUsingOrganizationsChart")
+    public String topUsingOrganizationsChart(@RequestParam(defaultValue = "" , value = "size") String size,
+                                            @RequestParam(defaultValue = "" , value = "page") String page){
+        System.out.println(size+"/"+page);
+        return "dashboard/reports/charts/topUsingOrganizationsChart";
+    }
+    @GetMapping("serviceUsageOverTimeChart")
+    public String serviceUsageOverTimeChart(@RequestParam(defaultValue = "" , value = "size") String size,
+                                            @RequestParam(defaultValue = "" , value = "page") String page){
+        System.out.println(size+"/"+page);
+        return "dashboard/reports/charts/serviceUsageOverTimeChart";
+    }
+    @GetMapping("topRequestedServicesChart")
+    public String topRequestedServicesChart(@RequestParam(defaultValue = "" , value = "size") String size,
+                                            @RequestParam(defaultValue = "" , value = "page") String page){
+        System.out.println(size+"/"+page);
+        return "dashboard/reports/charts/topRequestedServicesChart";
+    }
+    @GetMapping("usageRatioBetweenOrganizationsChart")
+    public String usageRatioBetweenOrganizationsChart(@RequestParam(defaultValue = "" , value = "size") String size,
+                                            @RequestParam(defaultValue = "" , value = "page") String page){
+        System.out.println(size+"/"+page);
+        return "dashboard/reports/charts/usageRatioBetweenOrganizationsChart";
+    }
+    @GetMapping("requestRatioByStatusChart")
+    public String requestRatioByStatusChart(@RequestParam(defaultValue = "" , value = "size") String size,
+                                                      @RequestParam(defaultValue = "" , value = "page") String page){
+        System.out.println(size+"/"+page);
+        return "dashboard/reports/charts/requestRatioByStatusChart";
+    }
+    @GetMapping("criticalServiceStatusChart")
+    public String criticalServiceStatusChart(@RequestParam(defaultValue = "" , value = "size") String size,
+                                                      @RequestParam(defaultValue = "" , value = "page") String page){
+        System.out.println(size+"/"+page);
+        return "dashboard/reports/charts/criticalServiceStatusChart";
+    }
+    @GetMapping("totalServiceRequestsChart")
+    public String totalServiceRequestsChart(@RequestParam(defaultValue = "" , value = "size") String size,
+                                                      @RequestParam(defaultValue = "" , value = "page") String page){
+        System.out.println(size+"/"+page);
+        return "dashboard/reports/charts/totalServiceRequestsChart";
+    }
+    @GetMapping("top10IntegratedUsersChart")
+    public String top10IntegratedUsersChart(@RequestParam(defaultValue = "" , value = "size") String size,
+                                                      @RequestParam(defaultValue = "" , value = "page") String page){
+        System.out.println(size+"/"+page);
+        return "dashboard/reports/charts/top10IntegratedUsersChart";
+    }
 }
